150e641d2e41791f0229a2c199fe7205688f4c6c
VasiliyDenisov <mr.v.m.denisov@mail.ru>
2023-11-07 21:25:04 +0300

    update

--- lua/base.lua
+++ lua/base.lua
@@ -10,7 +10,7 @@ vim.opt.title = true
 vim.opt.autoindent = true
 vim.opt.hlsearch = true
 vim.opt.backup = false
-vim.opt.showcmd = true
+vim.opt.showcmd = false
 vim.opt.cmdheight = 1
 vim.opt.laststatus = 2
 vim.opt.expandtab = true
@@ -24,7 +24,7 @@ vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.ai = true
 vim.opt.si = true
-vim.opt.wrap = false
+vim.opt.wrap = true
 vim.opt.backspace = { 'start', 'eol', 'indent' }
 vim.opt.shell = '/bin/bash'
 
